586428a1b1b43433b256abe6602dc15e0520d7b7
Michael Aquilina <michaelaquilina@gmail.com>
2025-01-29 10:56:30 +0000

    Remove extra "group title" from settings and reduce redundancy

--- prefs.js
+++ prefs.js
@@ -14,17 +14,29 @@ import {
 } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
 export default class WorkspaceLayoutPref extends ExtensionPreferences {
+  addWithLabel(label, widget) {
+    let labelWidget = new Gtk.Label({
+      label: label,
+      halign: Gtk.Align.START,
+      use_markup: true,
+    });
+    this.prefsWidget.attach(labelWidget, 0, this.currentRow, 2, 1);
+    this.prefsWidget.attach(widget, 2, this.currentRow, 2, 1);
+
+    this.currentRow++;
+  }
+
   fillPreferencesWindow(window) {
     window.settings = this.getSettings();
     const page = new Adw.PreferencesPage();
 
     const group = new Adw.PreferencesGroup({
-      title: _("Group Title"),
+      title: _("<b>Improved Workspace Indicator Preferences</b>"),
     });
-    let prefsWidget;
 
     // gtk4 apps do not have a margin property
-    prefsWidget = new Gtk.Grid({
+    this.currentRow = 0;
+    this.prefsWidget = new Gtk.Grid({
       margin_start: 18,
       margin_end: 18,
       margin_top: 18,
@@ -33,31 +45,15 @@ export default class WorkspaceLayoutPref extends ExtensionPreferences {
       row_spacing: 12,
     });
 
-    let title = new Gtk.Label({
-      label: "<b>Improved Workspace Indicator Preferences</b>",
-      halign: Gtk.Align.START,
-      use_markup: true,
-    });
-
-    prefsWidget.attach(title, 0, 0, 2, 1);
-
     // Panel Position Chooser
-
-    let panel_position_label = new Gtk.Label({
-      label: "Panel Position",
-      halign: Gtk.Align.START,
-    });
-
     let panel_position_combo = new Gtk.ComboBoxText();
     panel_position_combo.append("left", "left");
     panel_position_combo.append("right", "right");
     panel_position_combo.append("center", "center");
-
     panel_position_combo.active_id =
       window.settings.get_string("panel-position");
 
-    prefsWidget.attach(panel_position_label, 0, 1, 2, 1);
-    prefsWidget.attach(panel_position_combo, 2, 1, 2, 1);
+    this.addWithLabel("Panel Position", panel_position_combo);
 
     window.settings.bind(
       "panel-position",
@@ -67,15 +63,6 @@ export default class WorkspaceLayoutPref extends ExtensionPreferences {
     );
 
     // Skip Taskbar Mode Selector
-
-    let skip_taskbar_mode_label = new Gtk.Label({
-      label:
-        "Ignore Taskbar-Skipped Windows\r" +
-        "<small>These include hidden windows from the desktop-icons-ng extension.</small>",
-      halign: Gtk.Align.START,
-      use_markup: true,
-    });
-
     let skip_taskbar_mode_toggle = new Gtk.Switch({
       active: window.settings.get_boolean("skip-taskbar-mode"),
       valign: Gtk.Align.CENTER,
@@ -83,8 +70,11 @@ export default class WorkspaceLayoutPref extends ExtensionPreferences {
       visible: true,
     });
 
-    prefsWidget.attach(skip_taskbar_mode_label, 0, 2, 2, 1);
-    prefsWidget.attach(skip_taskbar_mode_toggle, 2, 2, 2, 1);
+    this.addWithLabel(
+      "Ignore Taskbar-Skipped Windows\r" +
+        "<small>These include hidden windows from the desktop-icons-ng extension.</small>",
+      skip_taskbar_mode_toggle,
+    );
 
     window.settings.bind(
       "skip-taskbar-mode",
@@ -95,19 +85,16 @@ export default class WorkspaceLayoutPref extends ExtensionPreferences {
 
     // Enable / Disable change on click
 
-    let change_on_click_label = new Gtk.Label({
-      label: "Change workspace on indicator click",
-      halign: Gtk.Align.START,
-    });
-
     let change_on_click_toggle = new Gtk.Switch({
       active: window.settings.get_boolean("change-on-click"),
       halign: Gtk.Align.END,
       visible: true,
     });
 
-    prefsWidget.attach(change_on_click_label, 0, 3, 2, 1);
-    prefsWidget.attach(change_on_click_toggle, 2, 3, 2, 1);
+    this.addWithLabel(
+      "Change workspace on indicator click",
+      change_on_click_toggle,
+    );
 
     window.settings.bind(
       "change-on-click",
@@ -118,19 +105,16 @@ export default class WorkspaceLayoutPref extends ExtensionPreferences {
 
     // Scroll to change workspace
 
-    let change_on_scroll_label = new Gtk.Label({
-      label: "Change workspace on indicator mouse-scroll",
-      halign: Gtk.Align.START,
-    });
-
     let change_on_scroll_toggle = new Gtk.Switch({
       active: window.settings.get_boolean("change-on-scroll"),
       halign: Gtk.Align.END,
       visible: true,
     });
 
-    prefsWidget.attach(change_on_scroll_label, 0, 4, 2, 1);
-    prefsWidget.attach(change_on_scroll_toggle, 2, 4, 2, 1);
+    this.addWithLabel(
+      "Change workspace on indicator mouse-scroll",
+      change_on_scroll_toggle,
+    );
 
     window.settings.bind(
       "change-on-scroll",
@@ -141,18 +125,16 @@ export default class WorkspaceLayoutPref extends ExtensionPreferences {
 
     // Scroll wraparound
 
-    let wrap_scroll_label = new Gtk.Label({
-      label: "Wraparound workspace when mouse-scroll",
-      halign: Gtk.Align.START,
-    });
     let wrap_scroll_toggle = new Gtk.Switch({
       active: window.settings.get_boolean("wrap-scroll"),
       halign: Gtk.Align.END,
       visible: true,
     });
 
-    prefsWidget.attach(wrap_scroll_label, 0, 5, 2, 1);
-    prefsWidget.attach(wrap_scroll_toggle, 2, 5, 2, 1);
+    this.addWithLabel(
+      "Wraparound workspace when mouse-scroll",
+      wrap_scroll_toggle,
+    );
 
     window.settings.bind(
       "wrap-scroll",
@@ -163,18 +145,16 @@ export default class WorkspaceLayoutPref extends ExtensionPreferences {
 
     // Hide Activities
 
-    let hide_activties_label = new Gtk.Label({
-      label: "Hide Activities Widget in Status Bar",
-      halign: Gtk.Align.START,
-    });
     let hide_activities_toggle = new Gtk.Switch({
       active: window.settings.get_boolean("hide-activities"),
       halign: Gtk.Align.END,
       visible: true,
     });
 
-    prefsWidget.attach(hide_activties_label, 0, 6, 2, 1);
-    prefsWidget.attach(hide_activities_toggle, 2, 6, 2, 1);
+    this.addWithLabel(
+      "Hide Activities Widget in Status Bar",
+      hide_activities_toggle,
+    );
 
     window.settings.bind(
       "hide-activities",
@@ -265,7 +245,7 @@ export default class WorkspaceLayoutPref extends ExtensionPreferences {
       css_filechooser_open();
     });
 
-    prefsWidget.connect("unrealize", () => {
+    this.prefsWidget.connect("unrealize", () => {
       let custom_css_dest = css_entry_buffer.get_text();
       if (custom_css_dest !== window.settings.get_string("custom-css-path")) {
         if (
@@ -280,11 +260,11 @@ export default class WorkspaceLayoutPref extends ExtensionPreferences {
     custom_css_box.append(custom_css_entry);
     custom_css_box.append(custom_css_button);
 
-    prefsWidget.attach(custom_css_label, 0, 7, 2, 1);
-    prefsWidget.attach(custom_css_help, 1, 7, 1, 1);
-    prefsWidget.attach(custom_css_box, 2, 7, 2, 1);
+    this.prefsWidget.attach(custom_css_label, 0, this.currentRow, 2, 1);
+    this.prefsWidget.attach(custom_css_help, 1, this.currentRow, 1, 1);
+    this.prefsWidget.attach(custom_css_box, 2, this.currentRow, 2, 1);
 
-    group.add(prefsWidget);
+    group.add(this.prefsWidget);
     page.add(group);
 
     window.add(page);
